5d32aa072669efcfd5de4aacc0f0b3aecf1baf89
Shankar <shivashankar.somabattina@gmail.com>
2019-10-07 14:10:51 +0530

    Add files via upload

---
+++ aksing questions.py
@@ -0,0 +1,8 @@
+print("How old are you ?", end =' ')
+age = input()
+print("How tall are you?", end =' ')
+height = input()
+print("What do you like?", end=' ')
+like = input()
+
+print(f'you are {age} years old, {height} cm tall and you like {like} ')
---
+++ promoting people.py
@@ -0,0 +1,5 @@
+age =input("How old are you?")
+height = input("How tall you are?")
+weight = input("How much you weigh?")
+
+print(f"So you are {age} years old, your height is {height} cm and you weigh {weight} kgs")
